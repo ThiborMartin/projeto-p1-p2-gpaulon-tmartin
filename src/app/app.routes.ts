b379ffef566203110a2b4f340993a0732b4ac528
Thibor de Mattos Martin <142036413+ThiborMartin@users.noreply.github.com>
2025-03-20 22:38:28 -0300

    Criação dos componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { BuscaComponent } from './busca/busca.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { CestaComponent } from './cesta/cesta.component';
+import { DetalheComponent } from './detalhe/detalhe.component';
+import { LoginComponent } from './login/login.component';
+import { VitrineComponent } from './vitrine/vitrine.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path:"", component:VitrineComponent},
+    {path:"busca", component:BuscaComponent},
+    {path:"cadastro", component:CadastroComponent},
+    {path:"cesta", component:CestaComponent},
+    {path:"detalhe", component:DetalheComponent},
+    {path:"login", component:LoginComponent}
+];
---
+++ src/app/cesta/cesta.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cesta',
+  imports: [],
+  templateUrl: './cesta.component.html',
+  styleUrl: './cesta.component.css'
+})
+export class CestaComponent {
+
+}
